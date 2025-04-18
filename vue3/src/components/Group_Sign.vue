e4f6d655c66bfcde5b809560c02e45c17bb23299
rikur <rikurzt@gmail.com>
2025-04-19 03:19:42 +0800

    update Club_info_SignUp

--- vue3/src/components/Group_Sign.vue
+++ vue3/src/components/Group_Sign.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <img src="/img/攤位報名重要日程表.png">
+  <img class="img-fluid" src="/img/攤位報名重要日程表.png">
 </template>
 
 <style scoped>
--- vue3/src/views/ClubInfo_ Signup.vue
+++ vue3/src/views/ClubInfo_ Signup.vue
@@ -14,5 +14,22 @@ import Group_Sign from "../components/Group_Sign.vue";
 </template>
 
 <style scoped>
+iframe {
+  width: 100%; /* 預設寬度為 100% */
+
+}
+
+@media only screen and (min-width: 1920px) {
+  iframe {
+    width: 1920px;
+
+  }
+}
+
+@media only screen and (max-width: 400px) {
+  iframe {
+    width: 400px;
+  }
+}
 
 </style>
