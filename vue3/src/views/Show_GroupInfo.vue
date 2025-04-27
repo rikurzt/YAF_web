680cda3d5673b59502886d22d9e30fa33368ae2c
rikur <rikurzt@gmail.com>
2025-04-28 03:11:15 +0800

    add ContentBlock.vue scroll effect

--- vue3/src/views/Show_GroupInfo.vue
+++ vue3/src/views/Show_GroupInfo.vue
@@ -4,7 +4,7 @@ import ContentBlock from "../components/ContentBlock.vue";
 </script>
 
 <template>
-  <ContentBlock title="表演團體" :force-visible="true" :force-visible="true"/>
+  <ContentBlock title="表演團體" :force-visible="true" />
 </template>
 
 <style scoped>
